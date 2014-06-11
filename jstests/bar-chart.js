af59019e89f432f8858d205df58e714111b062e1
jargonautical <lucy.knight@gmail.com>
2014-06-11 21:03:48 +0100

    Update bar-chart.js
    finding errors

--- jstests/bar-chart.js
+++ jstests/bar-chart.js
@@ -24,7 +24,7 @@
 
 var data = [24, 15, 18, 23, 41, 13];
 
-var chart = d3.select('div,#ilearning").append("svg)
+var chart = d3.select("div,#ilearning").append("svg)
   .attr("class", "chart")
   .attr("width", 400)
   .attr("height", 20 * data.length + 40)
