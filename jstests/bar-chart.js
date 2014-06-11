2ab7776ff4b743664205df168d25db47d9d0f719
jargonautical <lucy.knight@gmail.com>
2014-06-11 20:57:32 +0100

    Update bar-chart.js
    Added html

--- jstests/bar-chart.js
+++ jstests/bar-chart.js
@@ -1,3 +1,26 @@
+<html>
+  <!-- comment -->
+  <head>
+      <title> simple bar chart supposed to appear here somewhere</title>
+      <Link type="text.css" href="css.css" rel="stylesheet" />
+      <script type="text/javascript" src="d3.v3.min.js"></script>
+      <!-- <script src="http://d3js.org/d3.v3.min.js"></script> --><!-- latest version from d3 website -->
+    </head>
+    <body>
+      <h1> Header</h1>
+      <br>
+      <h2>Header2</h2>
+      <div>
+        <p class="Friendly-paragraph">
+          A simple bar chart.
+        </p>
+        </br>
+        <div id="ilearning">
+      </div>
+    </body>
+    <script type="text/javascript" src="js.js"></script>
+    </html>
+
 var data = [24, 15, 18, 23, 41, 13];
 
 var chart = d3.select('div,#ilearning").append("svg)
